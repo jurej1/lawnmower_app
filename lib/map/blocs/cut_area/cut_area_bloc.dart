04dc2e214f3a474f1af95314024adbdb3f92d589
Jure Jures <jure.04jj@gmail.com>
2024-02-07 11:27:42 +0100

    cleanup

--- lib/map/blocs/cut_area/cut_area_bloc.dart
+++ lib/map/blocs/cut_area/cut_area_bloc.dart
@@ -137,7 +137,7 @@ class CutAreaBloc extends Bloc<CutAreaEvent, CutAreaState> {
       List<LatLng> points = await _firebaseRepository.getCutArea();
 
       LatLng homeBaseLocation = await _firebaseRepository.getHomeBaseGPS();
-      List<LatLng> path = await _firebaseRepository.getCutPath();
+      // List<LatLng> path = await _firebaseRepository.getCutPath();
 
       List<MarkerShort> markers = points
           .map(
@@ -150,6 +150,7 @@ class CutAreaBloc extends Bloc<CutAreaEvent, CutAreaState> {
           )
           .toList();
 
+      final path = _polyRepository.generatePathInsidePolygon(points, 1);
       emit(
         state.copyWith(
           markers: markers,
