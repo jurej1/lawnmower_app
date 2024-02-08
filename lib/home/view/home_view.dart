403a79f481e08367ad279cf9e9db973c23b9a299
Jure Jures <jure.04jj@gmail.com>
2024-02-08 13:09:29 +0100

    chor: cleanup

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,7 +1,5 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 import 'package:lawnmower_app/bluetooth/view/bluetooth_view.dart';
 import 'package:lawnmower_app/home/blocs/blocs.dart';
 import 'package:lawnmower_app/home/widgets/widgets.dart';
