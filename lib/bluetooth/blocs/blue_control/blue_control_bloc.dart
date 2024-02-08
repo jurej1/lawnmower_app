2413b5ae648e95a677e20d6b82f716a7423eb075
Jure Jures <jure.04jj@gmail.com>
2024-02-08 13:37:53 +0100

    chor Cleanup

--- lib/bluetooth/blocs/blue_control/blue_control_bloc.dart
+++ lib/bluetooth/blocs/blue_control/blue_control_bloc.dart
@@ -130,7 +130,7 @@ class BlueControlBloc extends Bloc<BlueControlEvent, BlueControlState> {
   FutureOr<void> _mapUpwardToState(BlueControlUpwardPressed event, Emitter<BlueControlState> emit) async {
     try {
       if (state.isCharacteristicLoaded) {
-        String textToSend = "Dir: [x:100]";
+        String textToSend = "Dir: [x:100, y:0]";
         List<int> data = utf8.encode(textToSend);
         await state.writeCharacteristic!.write(data, withoutResponse: true);
       }
@@ -142,7 +142,7 @@ class BlueControlBloc extends Bloc<BlueControlEvent, BlueControlState> {
   FutureOr<void> _mapDownwardToState(BlueControlDownwardPressed event, Emitter<BlueControlState> emit) async {
     try {
       if (state.isCharacteristicLoaded) {
-        String textToSend = "Dir: [x:-100]";
+        String textToSend = "Dir: [x:-100, y:0]";
         List<int> data = utf8.encode(textToSend);
         await state.writeCharacteristic!.write(data, withoutResponse: true);
       }
@@ -154,7 +154,7 @@ class BlueControlBloc extends Bloc<BlueControlEvent, BlueControlState> {
   FutureOr<void> _mapLeftToState(BlueControlLeftPressed event, Emitter<BlueControlState> emit) async {
     try {
       if (state.isCharacteristicLoaded) {
-        String textToSend = "Dir: [y:-100]";
+        String textToSend = "Dir: [x:0 ,y:-100]";
         List<int> data = utf8.encode(textToSend);
         await state.writeCharacteristic!.write(data, withoutResponse: true);
       }
@@ -166,7 +166,7 @@ class BlueControlBloc extends Bloc<BlueControlEvent, BlueControlState> {
   FutureOr<void> _mapRightToState(event, Emitter<BlueControlState> emit) async {
     try {
       if (state.isCharacteristicLoaded) {
-        String textToSend = "Dir: [y:100]";
+        String textToSend = "Dir: [x:0, y:100]";
         List<int> data = utf8.encode(textToSend);
         await state.writeCharacteristic!.write(data, withoutResponse: true);
       }
