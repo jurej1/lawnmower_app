49bded7e58ce2e93b810d2665a355e7c81c7de9e
Jure Jures <jure.04jj@gmail.com>
2024-02-08 18:05:53 +0100

    chor: cleanup

--- packages/firebase_repository/lib/src/firebase_repository.dart
+++ packages/firebase_repository/lib/src/firebase_repository.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:firebase_database/firebase_database.dart';
@@ -10,10 +8,6 @@ class FirebaseRepository {
   FirebaseRepository({FirebaseDatabase? firebaseDatabase}) : _database = firebaseDatabase ?? FirebaseDatabase.instance;
 
   final FirebaseDatabase _database;
-
-  final _firebaseURL = "https://lawnmower-825c3-default-rtdb.europe-west1.firebasedatabase.app/";
-
-  String get firebaseUrl => _firebaseURL;
 
   Future<DataSnapshot> getBatteryVal() async {
     return _database.ref("battery").get();
@@ -27,9 +21,7 @@ class FirebaseRepository {
             })
         .toList();
 
-    final jsonString = jsonEncode(jsonData);
-
-    return _database.ref("cut_area").set(jsonString);
+    return _database.ref("cut_area").set(jsonData);
   }
 
   Future<void> setCutPath(List<LatLng> path) async {
@@ -40,8 +32,7 @@ class FirebaseRepository {
             })
         .toList();
 
-    final jsonString = jsonEncode(jsonData);
-    return _database.ref("cut_path").set(jsonString);
+    return _database.ref("cut_path").set(jsonData);
   }
 
   Future<DataSnapshot> getCutArea() async {
@@ -50,27 +41,6 @@ class FirebaseRepository {
 
   Future<DataSnapshot> getCutPath() async {
     return _database.ref("cut_path").get();
-
-    // final response = await http.get(Uri.parse(url));
-
-    // if (response.statusCode == 200) {
-    //   final List<dynamic> jsonData = jsonDecode(response.body);
-
-    //   List<LatLng> result = jsonData.map((data) {
-    //     return LatLng(
-    //       data["lat"],
-    //       data["lng"],
-    //     );
-    //   }).toList();
-
-    //   return result;
-    // } else {
-    //   throw Exception('Failed to load cut path data');
-    // }
-  }
-
-  Future<DataSnapshot> getCurrentRobotLocation() async {
-    return _database.ref("GPS_current").get();
   }
 
   Future<DataSnapshot> getHomeBaseGPS() async {
